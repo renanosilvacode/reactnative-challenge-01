4e98e5c5a06a818e376dbf6b8a33427f5f11cdba
Renan Oliveira da Silva <renanosilva@gmail.com>
2020-10-07 00:26:46 -0300

    Implementing the Like function

--- src/App.js
+++ src/App.js
@@ -24,15 +24,38 @@ export default function App() {
 
   async function handleLikeRepository(id) {
     // Implement "Like Repository" functionality
+    const urlToPost = `repositories/${id}/like`;
+    /*
+      ////This was my first response. 
+      ////It passed on the test, however did not respected the concept of immutability
+      ////Also, it kept changing the index of the updated object :-/
+
+      await api.post(urlToPost).then(response => {
+      const repositoryUpdatedIndex = repositories.findIndex(repository => repository.id === id);
+      repositories.splice(repositoryUpdatedIndex, 1);
+      setRepositories([... repositories, response.data]);
+    });*/
+
+    //Here a more elegant solution using map to update the object on the fly.
+    const response = await api.post(urlToPost);
+    const likedRepository = response.data;
+
+    const repositoriesUpdated = repositories.map(repository => {
+      return repository.id === id ? likedRepository : repository;
+    })
+
+    setRepositories(repositoriesUpdated);
+
   }
 
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
-        
-        {repositories.map(repository => {
-          return (
+        <FlatList
+            data={repositories}
+            keyExtractor={repository => repository.id}
+            renderItem={({item: repository}) => (
               <View style={styles.repositoryContainer} key={repository.id}>
                 <Text style={styles.repository}>{repository.title}</Text>
 
@@ -51,7 +74,7 @@ export default function App() {
                   <Text
                     style={styles.likeText}
                     // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-                    testID={`repository-likes-1`}
+                    testID={`repository-likes-${repository.id}`}
                   >
                     {repository.likes} curtidas
                   </Text>
@@ -59,16 +82,15 @@ export default function App() {
 
                 <TouchableOpacity
                   style={styles.button}
-                  onPress={() => handleLikeRepository(1)}
+                  onPress={() => handleLikeRepository(repository.id)}
                   // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-                  testID={`like-button-1`}
+                  testID={`like-button-${repository.id}`}
                 >
-                  <Text style={styles.buttonText}>Like</Text>
+                  <Text style={styles.buttonText}>Curtir</Text>
                 </TouchableOpacity>
               </View>
-          )
-        })}
-
+            )}
+        />
       </SafeAreaView>
     </>
   );
